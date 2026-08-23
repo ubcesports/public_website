51ff5d0b441beea2b24cdd7d299df85246ada637
Sudipto Islam <sudipto.islam2005@gmail.com>
2026-08-23 14:12:09 +0300

    Fixed splash page to work with smaller screens

--- frontend/src/app/components/home_page/SplashPage.tsx
+++ frontend/src/app/components/home_page/SplashPage.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
-import Link from "next/link";
 import GradientButton from "../GradientButton";
 
 export default function SplashPage() {
   return (
-    <section className="relative h-svh w-full overflow-hidden">
+    <section className="relative min-h-svh w-full overflow-hidden">
       {/* Background image */}
       <Image
         src="/images/splash.jpg"
@@ -16,15 +15,15 @@ export default function SplashPage() {
       />
 
       {/* Content */}
-      <div className="absolute inset-0 z-10 flex flex-col items-center justify-center">
-        <div className="flex w-full items-center justify-center gap-3">
+      <div className="relative z-10 flex min-h-svh flex-col items-center justify-center px-4 py-20">
+        <div className="flex w-full items-center justify-center gap-2 sm:gap-3">
           <Image
             src="/logo/logo_variant_1.png"
             alt=""
             width={322}
             height={313}
             priority
-            className="size-30"
+            className="h-auto w-20 md:w-30"
           />
           <Image
             src="/logo/logo_variant_text.png"
@@ -32,11 +31,11 @@ export default function SplashPage() {
             width={640}
             height={277}
             priority
-            className="h-auto w-70"
+            className="h-auto w-48 md:w-70"
           />
         </div>
 
-        <span className="text-white/77 [text-shadow:0_2.52px_2.84px_rgba(0,0,0,0.47)] uppercase font-bold tracking-widest mt-5 text-xl font-header">
+        <span className="text-white/77 [text-shadow:0_2.52px_2.84px_rgba(0,0,0,0.47)] uppercase font-bold tracking-widest mt-5 md:text-xl text-md font-header">
           presents
         </span>
 
@@ -46,14 +45,14 @@ export default function SplashPage() {
           width={1847}
           height={703}
           priority
-          className="h-auto w-200 mb-3"
+          className="mb-3 h-auto w-full max-w-200"
         />
 
-        <span className="bg-[#2D344A] text-bold text-2xl mb-6 p-2 font-mono">
+        <span className="mb-6 max-w-full bg-[#2D344A] p-2 text-center font-mono md:text-2xl text-lg text-bold">
           September 25TH, 2026 - AMS Nest Great Hall
         </span>
 
-        <GradientButton href="/events/liftoff" textSize="text-xl">
+        <GradientButton href="/events/liftoff" textSize="md:text-xl text-md">
           learn more
         </GradientButton>
       </div>
